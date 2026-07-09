7543dd83414eb42d53d54c296be0765378a79915
Mirzapur-Mandal-Sangat <satbhaktimuktisandesh1@gmail.com>
2026-07-10 01:51:03 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,36 @@
-const CACHE_NAME = 'mirzapur-mandal-v3';
+const CACHE_NAME = 'mirzapur-mandal-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
 
-self.addEventListener('fetch', event => {
+// Install: Cache files save karna
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch: Agar network na ho toh cache se dikhana
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        return response;
-      })
-      .catch(error => {
-        // अगर नेटवर्क फेल हुआ, तो मेन पेज को रिफ्रेश का सिग्नल भेजें
-        self.clients.matchAll().then(clients => {
-          clients.forEach(client => client.postMessage('reload_page'));
-        });
-        return caches.match(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate: Purana cache hatana
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
