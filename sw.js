928c89a4b65d17899e82f34f0226f7cad4753fbc
Mirzapur-Mandal-Sangat <satbhaktimuktisandesh1@gmail.com>
2026-07-10 03:29:58 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
 const CACHE_NAME = 'mirzapur-mandal-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS_TO_CACHE = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
 });
 
 self.addEventListener('fetch', (event) => {
